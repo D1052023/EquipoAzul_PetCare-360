53e29144397622eadebe0400dd38a3c621d328f4
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-09-26 08:26:54 -0500

    Estructurado de carpetas

---
+++ src/main/java/edo/dosw/reto/EquipoAzul_PetCare_360/EquipoAzulPetCare360Application.java
@@ -0,0 +1,13 @@
+package edo.dosw.reto.EquipoAzul_PetCare_360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoAzulPetCare360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipoAzulPetCare360Application.class, args);
+	}
+
+}
---
+++ src/test/java/edo/dosw/reto/EquipoAzul_PetCare_360/EquipoAzulPetCare360ApplicationTests.java
@@ -0,0 +1,13 @@
+package edo.dosw.reto.EquipoAzul_PetCare_360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipoAzulPetCare360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
