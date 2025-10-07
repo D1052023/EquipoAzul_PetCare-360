b74ca58cddd7c44abbf90d25285777cad0de1232
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-10-07 13:15:48 -0500

    Implementacio Swagger

--- src/main/java/edo/dosw/reto/EquipoAzul_PetCare_360/EquipoAzulPetCare360Application.java
+++ src/main/java/edo/dosw/reto/EquipoAzul_PetCare_360/EquipoAzulPetCare360Application.java
@@ -1,9 +1,17 @@
 package edo.dosw.reto.EquipoAzul_PetCare_360;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "edo.dosw.reto")
+@EnableMongoRepositories(basePackages = "edo.dosw.reto.repositories")
 public class EquipoAzulPetCare360Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/edo/dosw/reto/configs/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package edo.dosw.reto.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+/**
+ * Clase que configura la información que aparecerá en la documentación de la API.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Define un bean de tipo OpenAPI que sera utilizado por Springdoc OpenAPI
+     * para generar la documentación de la API automáticamente.
+     *
+     */
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PetCare-360 API")
+                        .description("API para la gestión de servicios de veterinarias")
+                        .version("1.0"));
+    }
+}
