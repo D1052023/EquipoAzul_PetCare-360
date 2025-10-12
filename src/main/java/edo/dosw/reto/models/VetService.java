a518ff1b455f5051c459ad1885744b0f51232355
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-10-11 21:00:03 -0500

    Implementacion registro

--- src/main/java/edo/dosw/reto/models/VetService.java
+++ src/main/java/edo/dosw/reto/models/VetService.java
@@ -1,5 +1,6 @@
 package edo.dosw.reto.models;
 
+import edo.dosw.reto.enums.ServiceType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class VetService {
     private String id;
-    private String name;
+    private ServiceType type;
     private String description;
 }
